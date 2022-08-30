3b5c7ae6f5013bde7d8546fd106e45534b8bb03a
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-30 20:14:08 +0300

    Interests section implementation

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Interests = () => {
   return (
-    <div>Interests</div>
+    <div className='interests'>
+        <h2 className='heading'>Interests</h2>
+        <p className='paragraph'>
+            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur.
+            Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+    </div>
   )
 }
 
