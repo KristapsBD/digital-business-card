4ed461a8119f79e18b6ee2cc8850f0b2a95dca57
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-30 17:33:09 +0300

    Implement info section

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import port from '../images/portrait.jpg'
+import { HiMail } from 'react-icons/hi';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Info = () => {
-  return (
-    <div>Info</div>
-  )
+    return (
+        <div className='info'>
+            <img src={port} alt='portrait' className='portrait' />
+            <div className='title'>
+                <h1>Kristaps Briks</h1>
+                <p>Frontend developer</p>
+                <small>University of Latvia</small>
+            </div>
+            <div className='buttons'>
+                <a href="mailto:user@inbox.lv" ><HiMail className='mail-icon'/>Email</a>
+                <a href="https://www.linkedin.com/" target="_blank"><FaLinkedin className='linked-icon'/>LinkedIn</a>
+            </div>
+        </div>
+    )
 }
 
 export default Info
