62889362254dd9b26e53fca45ed4d896eb70de49
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-30 20:47:25 +0300

    Footer section implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { AiFillTwitterSquare } from 'react-icons/ai'
+import { AiFillFacebook } from 'react-icons/ai'
+import { FaInstagramSquare } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className='footer'>
+            <a href='https://twitter.com/'><AiFillTwitterSquare /></a>
+            <a href='https://www.facebook.com/'><AiFillFacebook /></a>
+            <a href='https://www.instagram.com/'><FaInstagramSquare /></a>
+            <a href='https://github.com/'><FaGithubSquare /></a>
+        </div>
+    )
 }
 
 export default Footer
